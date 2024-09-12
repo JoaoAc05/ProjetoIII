db60f458bb38dd8c844d32e34aa707a16a248109
João Alves <joaoalvescordeiro2005@gmail.com>
2024-09-12 12:47:34 -0400

    remove comentarios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import createTable from './Controler/Aluno.js';
 
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(cors());
 
 import  rota from './rotas.js';
 app.use(rota);
-
+createTable();
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor HTTP rodando na porta 3000')
 });
